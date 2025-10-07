1bd10ee03c36403688f1aa549e5cf5032b7cfbbe
Alejandro Carrillo <carrilloalejandro0215@gmail.com>
2025-10-07 15:49:57 -0500

    Postgres resturning if user on db or not

--- server.js
+++ server.js
@@ -4,14 +4,33 @@ const PORT = 3000;
 app.use(express.static(__dirname)); // Serves all the files in the directory 
 app.use(express.json()); // We let server know we will send json to parse back to js
 
+// Postgres Setup
 
+const{Pool} = require('pg');
+const pool = new Pool({
+    user: 'alejandrocarrillo',
+    host: 'localhost',
+    database: 'thinkofu',
+    password : '',
+    port: 5432,
+});
 
 
 app.post('/checkuser', async(req,res) => {
     const{code,password} = req.body; // We read the body of the json sent
     try{
         console.log(code,password)
-        res.send("Correcto")
+        const result = await pool.query(
+            'SELECT * FROM users WHERE code = $1',
+            [code]
+        );
+        if(result.rows.length === 0){
+            console.log("usuario no encontrado")
+
+        }else{
+            console.log("User encontrado")
+            console.log(result.rows)
+        }
     }catch(err){
         console.log(err)
         res.status(500).send("Error")
